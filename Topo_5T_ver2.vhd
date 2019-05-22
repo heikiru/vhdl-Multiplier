da423b803e9fa812f86666b3d4b8ea23d7024900
Vitor Della Torre dos Santos <47284767+heikiru@users.noreply.github.com>
2019-05-22 07:50:54 -0300

    Soft Update Topo_5T_ver2

--- Topo_5T_ver2.vhd
+++ Topo_5T_ver2.vhd
@@ -1,32 +1,39 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-entity Topo_5T_ver2 is
-	port(
+ENTITY Topo_5T_ver2 IS -- 16 bits version!
+	PORT (
 	inicio, resetFSM: in std_logic;
 	entA, entB: in std_logic_vector(15 downto 0);
 	clk: in std_logic;
 	Pout: out std_logic_vector(31 downto 0)
 	);
-end Topo_5T_ver2;
+END Topo_5T_ver2;
 
-architecture arch_Topo of Topo_5T_ver2 is
-signal pronto, cargaA, cargaB, cargaP, muxSel, resetP, resetA, Az, Bz: std_logic;
-component is
-  port (
-		
+ARCHITECTURE arch_Topo of Topo_5T_ver2 IS
+SIGNAL pronto, cargaA, cargaB, cargaP, muxSel, resetP, resetA, Az, Bz: std_logic;
+COMPONENT BlocoControle_5T_ver2 IS 
+  PORT (
+    clk, init, resetFSM: in std_logic;
+    cargaA, cargaB, cargaP, cargaCont: in std_logic;
+    resetP: in std_logic; 
+    srRegP, srRegA: in std_logic; 
+    Az, Bz, ContZ: in std_logic; 
+    A_0: in std_logic; 
+    pronto: out std_logic
   );
-end component;
+END COMPONENT;
 
-component is
-	port(
+COMPONENT IS
+	PORT (
+	
 	);
-end component;
+END COMPONENT;
 
-begin
+BEGIN
 	
-	BC: BlocoControle port map(clk, pronto, cargaA, cargaB, cargaP, resetP, resetA, muxSel, inicio, resetFSM, Az, Bz);
+	BC: BlocoControle_5T_ver2 PORT MAP( -- );
 	
-	BO: BlocoOperativo_16Bits port map(clk, entA, entB, cargaA, cargaB, cargaP, resetP, resetA, muxSel, Az, Bz, Pout);
+	BO: BlocoOperativo_16Bits PORT MAP(clk, entA, entB, cargaA, cargaB, cargaP, resetP, resetA, muxSel, Az, Bz, Pout);
 	
-end arch_Topo;
+END arch_Topo;
