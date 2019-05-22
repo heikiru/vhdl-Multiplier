fc80fb8b3eeb9e9e1f675a991219a25bf2ff7c54
Vitor Della Torre dos Santos <47284767+heikiru@users.noreply.github.com>
2019-05-22 07:28:35 -0300

    Update on BlocoControle_5T_ver2
    Finished the block.

--- BlocoControle_5T_ver2.vhd
+++ BlocoControle_5T_ver2.vhd
@@ -3,19 +3,24 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY BlocoControle_5T_ver2 IS
-  port (
+  PORT (
     clk, init, resetFSM: in std_logic;
     cargaA, cargaB, cargaP, cargaCont: in std_logic;
-    resetA, resetB, resetP, resetCont: in std_logic;
-    Az, Bz: in std_logic;
-    pronto: out std_logic;
+    resetA, resetB, resetP, resetCont: in std_logic; -- resetP can be taken later, since we are using a Mux
+    srRegP, srRegA: in std_logic; -- Shift Right inputs to P and A
+    Az, Bz, ContZ: in std_logic; -- verifies if A, B and Cont are zero in certain situations
+    pronto: out std_logic
   );
+    
+    -- Still needs to get the first 8 bits from register P. 
+    -- In this case, we are treating P like only one register, although the layout of the project asked for two.
   
-end BlocoControle_5T_ver2;
+END BlocoControle_5T_ver2;
 
 ARCHITECTURE arch_BC_5T_ver2 IS
 TYPE state IS (S0, S1, S2, S3, S4, S5, S6);
-STATE EA, PE: state;
+SIGNAL EA, PE: state;
+SIGNAL A_0: std_logic;
 
 BEGIN
   PROCESS(clk, EA)
@@ -38,11 +43,33 @@ BEGIN
           cargaB <= '1';
           cargaCont <= '1';
           resetP <= '1';
+          PE <= S2;
         WHEN S2 =>
-          IF A
+            cargaA <= '0';
+            cargaB <= '0';
+            cargaCont <= '0';
+            resetP <= '0';
+            IF Az = '1' OR Bz = '1' THEN
+              PE <= S6;
+            ELSE 
+              PE <= S3;
         WHEN S3 =>
+            IF ContZ = '1' THEN
+              PE <= S6;
+            ELSE 
+              IF A_0 = '1' THEN
+                PE <= S4;
+              ELSE
+                PE <= S5;
         WHEN S4 =>
+            cargaP <= '1';
+            PE <= S5;
         WHEN S5 =>
+            cargaP <= '0';
+            srRegP <= '1';
+            srRegA <= '1';
+            CargaCont <= '1';
+            PE <= S3;
         WHEN S6 =>
-    
--- Still to finish
+            pronto <= '1';
+            PE <= S0;
